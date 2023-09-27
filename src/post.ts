d392de103aa94de5073c05aec8e610ebaf386ce4
Jean Simon Otis <jsotis@chainsawesomegames.com>
2023-09-27 08:53:28 -0400

    Test to retry caching in post if Library folder is locked

--- src/post.ts
+++ src/post.ts
@@ -6,15 +6,24 @@ import { isErrorLike } from './lib/isErrorLike'
 import log from './lib/log'
 
 async function post(): Promise<void> {
-  try {
-    const { cacheDir, targetPath, cachePath } = getVars()
+  for (let i = 0; i <= 5; i++) {
+    await delay(2000 * i)
+    try {
+      const { cacheDir, targetPath, cachePath } = getVars()
+  
+      await mkdirP(cacheDir)
+      await mv(targetPath, cachePath, { force: true })
+    } catch (error: unknown) {
+      log.trace(error)
+      if (i < 5) continue;
+      setFailed(isErrorLike(error) ? error.message : `unknown error: ${error}`)
+    }
+    console.log('post')
+  }  
+}
 
-    await mkdirP(cacheDir)
-    await mv(targetPath, cachePath, { force: true })
-  } catch (error: unknown) {
-    log.trace(error)
-    setFailed(isErrorLike(error) ? error.message : `unknown error: ${error}`)
-  }
+function delay(ms: number) : Promise<void> {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
 void post()
